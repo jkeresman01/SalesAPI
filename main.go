85cd7777d8005a821c5bcca24dd7af6e84e4d892
josip <josipkeresman@gmail.com>
2025-01-12 19:48:43 +0100

    Add db connection logic

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
-package main;
+package main
 
 import (
-    "github.com/gofiber/fiber/v2"
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
 )
 
+var DB *gorm.DB
+
 func main() {
-    app := fiber.New()
+	godotenv.Load()
 
-    app.Get("/", func(ctx *fiber.Ctx) error {
-        return ctx.Status(200).JSON(fiber.Map {
-            "success" : true,
-            "message" : "Go fiber first api created",
-        })
-    });
+	dbHost := os.Getenv("MYSQL_HOST ")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	dbUser := os.Getenv("MYSQL_USER")
+	dbName := os.Getenv("MYSQL_DBNAME")
 
-    app.Listen(":3000")
+	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
+	var db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+
+	if err != nil {
+		panic("MYSQL databse connection has failed")
+	}
+
+	DB = db
+	fmt.Printf("Successful connection to %s", dbName)
+
 }
-
-
-
