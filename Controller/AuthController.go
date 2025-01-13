44d551831d8ffe3499a1f23bc600c8e61e0c68e2
josip <josipkeresman@gmail.com>
2025-01-13 01:08:43 +0100

    Add JWT login auth logic

--- Controller/AuthController.go
+++ Controller/AuthController.go
@@ -1,9 +1,79 @@
 package Controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	db "github.com/jkeresman01/SalesAPI/Config"
+	"github.com/jkeresman01/SalesAPI/Model"
+	"os"
+	"strconv"
+	"time"
+)
 
 func Login(c *fiber.Ctx) error {
-	return nil
+	cashierId := c.Params("cashierId")
+
+	var data map[string]string
+
+	err := c.BodyParser(&data)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"success": false,
+			"message": "Invalid post request",
+		})
+	}
+
+	if data["passcode"] == "" {
+		return c.Status(400).JSON(fiber.Map{
+			"success": false,
+			"message": "Passcode is required",
+			"error":   map[string]interface{}{},
+		})
+	}
+
+	var cashier Model.Cashier
+
+	db.DB.Where("id=?", cashierId).First(&cashier)
+
+	if cashier.Id == 0 {
+		return c.Status(401).JSON(fiber.Map{
+			"success": false,
+			"message": "Cashier not found",
+			"error":   map[string]interface{}{},
+		})
+	}
+
+	if cashier.Passcode != data["passcode"] {
+		return c.Status(401).JSON(fiber.Map{
+			"success": false,
+			"message": "Passcode not matched",
+			"error":   map[string]interface{}{},
+		})
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Issuer":    strconv.Itoa(int(cashier.Id)),
+		"ExpiredAt": time.Now().Add(time.Hour * 24),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		return c.Status(401).JSON(fiber.Map{
+			"success": false,
+			"message": "Token expired or invalid",
+		})
+	}
+
+	cashierData := make(map[string]interface{})
+	cashierData["token"] = tokenString
+
+	return c.Status(200).JSON(fiber.Map{
+		"success": true,
+		"message": "Success",
+		"data":    cashierData,
+	})
 }
 
 func Logout(c *fiber.Ctx) error {
